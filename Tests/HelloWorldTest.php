4373c08292d50a0526918203112eb08b600981a0
pandurx <internalfire5@live.com>
2018-06-07 14:47:26 -0400

    update

--- Tests/HelloWorldTest.php
+++ Tests/HelloWorldTest.php
@@ -2,8 +2,22 @@
 
 class HelloWorldTest extends PHPUnit_Framework_TestCase
 {
+    /**
+     * @var PDO
+     */
+    private $pdo;
 
+    public function setUp()
+    {
+        $this->pdo = new PDO($GLOBALS['db_dsn'], $GLOBALS['db_username'], $GLOBALS['db_password']);
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->pdo->query("CREATE TABLE hello (what VARCHAR(50) NOT NULL)");
+    }
 
+    public function tearDown()
+    {
+        $this->pdo->query("DROP TABLE hello");
+    }
 
     public function testHelloWorld()
     {
